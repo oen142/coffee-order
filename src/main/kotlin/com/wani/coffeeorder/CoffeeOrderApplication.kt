152f47f0112dc5aae20cd2facae9e0947a6bc47c
oen142 <oen142@naver.com>
2021-06-16 23:19:13 +0900

    Initial commit

---
+++ src/main/kotlin/com/wani/coffeeorder/CoffeeOrderApplication.kt
@@ -0,0 +1,11 @@
+package com.wani.coffeeorder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoffeeOrderApplication
+
+fun main(args: Array<String>) {
+    runApplication<CoffeeOrderApplication>(*args)
+}
---
+++ src/test/kotlin/com/wani/coffeeorder/CoffeeOrderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wani.coffeeorder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoffeeOrderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
